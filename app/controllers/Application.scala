4d92f600108925d4e831927c01ff91d81aed3e94
Meredith Leu <meredith.leu@gmail.com>
2014-06-12 17:30:13 -0700

    Add minimal functionality

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,36 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import scalax.io._
+import java.nio.file.{Paths, Files}
+import scala.sys.process._
+import java.io.File
+
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index(tex: String) = Action {
+
+    val outputPdfName = "template.pdf"
+    val templateFileName = "conf/template.tex"
+    val templateFile = new File(templateFileName)
+    assert(Files.exists(Paths.get(templateFileName)))
+
+    val texString = "\\def\\formula{" + tex + "}\\input{" + templateFile.getAbsolutePath + "}"
+
+    val outputImageName = tex + ".png"
+
+    val toPdf = Process("pdflatex " + texString) !!
+
+    Logger.info(toPdf)
+
+    val toPng = Process("convert -density 300 " + outputPdfName + " -quality 90 " + outputImageName) !!
+
+    Logger.info(toPng)
+
+    assert(Files.exists(Paths.get(outputImageName)))
+    val outputImage: Output = Resource.fromFile(outputImageName)
+
+    Ok.sendFile(content = new File(outputImageName), inline = true)
   }
 
 }
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -4,7 +4,7 @@ import org.junit.runner._
 
 import play.api.test._
 import play.api.test.Helpers._
-
+import java.net.URLEncoder.encode
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
@@ -15,20 +15,28 @@ class ApplicationSpec extends Specification {
 
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
-      route(FakeRequest(GET, "/boum")) must beNone
-    }
-
-    "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
-
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
-    }
+//    "send 404 on a bad request" in new WithApplication{
+//      route(FakeRequest(GET, "/boum")) must beNone
+//    }
+//
+//    "render the index page" in new WithApplication{
+//      val home = route(FakeRequest(GET, "/")).get
+//
+//      status(home) must equalTo(OK)
+//      contentType(home) must beSome.which(_ == "text/html")
+//      contentAsString(home) must contain ("Your new application is ready.")
+//    }
 
     "return an image for valid LaTeX" in new WithApplication{
-      val tex = "E=\\frac{m_1v^2}{2}"
+      val tex = encode("E=\\frac{m_1v^2}{2}", "UTF-8")
+      val anyJax = route(FakeRequest(GET, "/"+tex)).get
+      status(anyJax) must equalTo(OK)
+      contentType(anyJax) must beSome.which(_ == "image/png")
+      // the title must contain the tex sent over
+    }
+
+    "return another image for valid LaTeX" in new WithApplication{
+      val tex = encode("\\frac{Sales}{Traffic Out}", "UTF-8")
       val anyJax = route(FakeRequest(GET, "/"+tex)).get
       status(anyJax) must equalTo(OK)
       contentType(anyJax) must beSome.which(_ == "image/png")
